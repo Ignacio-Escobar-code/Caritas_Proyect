5899995c8d7b927fa72b1a3e3a24bafefc84b901
Nacho <nachoescobar.ok@gmail.com>
2023-02-02 18:46:34 -0300

    creando componentes

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,11 @@
+import SubscribeForm from "./components/Form-send-mail/form_mail";
 import Navbar from "./components/Navbar/navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SubscribeForm />
     </div>
   );
 }

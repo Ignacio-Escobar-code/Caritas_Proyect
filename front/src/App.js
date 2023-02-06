14c85c264763ab12dac7cc2f718ac2a0998ac8da
Nacho <nachoescobar.ok@gmail.com>
2023-02-06 16:39:40 -0300

    actualizacion

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,5 @@
 import SubscribeForm from "./components/Suscribe-mail/suscribe_mail";
 import Navbar from "./components/Navbar/navbar";
-import {} from "react";
 
 function App() {
   return (
--- front/src/components/Form-send-mail/form_mail.jsx
+++ front/src/components/Form-send-mail/form_mail.jsx
@@ -1,1 +1,70 @@
+import React, { useState } from "react";
 
+const FormMail = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    country: "",
+    message: "",
+  });
+
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puede agregar su lógica de envío de formulario
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nombre:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="country">País:</label>
+        <input
+          type="text"
+          id="country"
+          name="country"
+          value={formData.country}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Mensaje:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button type="submit">Enviar</button>
+    </form>
+  );
+};
+
+export { FormMail };

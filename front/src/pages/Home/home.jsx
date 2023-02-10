f006d202d53089a8b6d1812307e9cf312e88f750
NachoEscobar <nachoescobar.ok@gmail.com>
2023-02-10 05:33:15 -0300

    Hice la mitad del home

--- front/src/pages/Home/home.jsx
+++ front/src/pages/Home/home.jsx
@@ -1,19 +1,38 @@
 import React from "react";
+import "./home.css";
 
 const Home = () => {
   return (
     <>
       <div className="homeSection1">
-        <h1>Buscas una agencia de Marketing?</h1>
+        {/* <h1>¿Buscas una agencia de Marketing?</h1> */}
+        <h1>
+          <img src="homeSection1.png" alt="Titulo con foto" />
+        </h1>
         <button type="submit">SOLICITA MAS INFO</button>
       </div>
       <div className="homeSection2">
-        <h2>La fórmula para triunfar en el marqueting digital</h2>
-        <p>
-          Se basa en entender a tus clientes potenciales en el formato correcto
-          y el momento adecuado
-        </p>
+        <div className="contenidoTextual">
+          <h2>La fórmula para triunfar en el marqueting digital</h2>
+          <p>
+            Se basa en entender a tus clientes potenciales en el formato
+            correcto y el momento adecuado
+          </p>
+        </div>
+        <img
+          src="CirculoDeCirculos.png"
+          alt="Fondo Decorativo"
+          className="circuloDeCirculos"
+        />
+        <div className="imagenesDecoLaptop">
+          <img
+            src="homeSection2Laptop.png"
+            alt="Imagen ilustrativa laptop"
+            className="laptopImg"
+          />
+        </div>
       </div>
+
       {/* aca van tarjetas */}
       <div>
         <h5>Auditoria Digital</h5>

e4a9fe23ead4f2bd3f3ba17090ccd91ebdcaa647
julian <j.armanino@live.com>
2023-02-14 18:30:20 -0300

    asesorias update 2.0

--- front/src/pages/Asesorias/asesorias.jsx
+++ front/src/pages/Asesorias/asesorias.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./asesorias.css";
 const Asesorias = () => {
   return (
     <>
-      <div>
+      <div className="as-Section1">
         <h1>Asesorias</h1>
         <p>
           Para emprendedores y pymes. Consectur elit. Lorem ipsum dolor sit
@@ -11,105 +11,112 @@ const Asesorias = () => {
         </p>
         <button type="submit">Solicita mas info</button>
       </div>
-      <div>
-        <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
-          obcaecati laudantium iste qui nulla rerum nihil doloribus, commodi
-          officiis aut minima in sit a, dolorum cupiditate voluptatum,
-          temporibus itaque ab.
-        </p>
-      </div>
+      <div className="as-Section2">
+        <div className="as-Section2-contenidoTextual">
+          <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+            obcaecati laudantium iste qui nulla rerum nihil doloribus, commodi
+            officiis aut minima in sit a, dolorum cupiditate voluptatum,
+            temporibus itaque ab.
+          </p>
+        </div>
         <div className="as-Section2-img">
           <img src="../as-section2-img.png" alt="foto de laptop con una lamparita encendida" />
         </div>
-      </div>
       <div className="overlap-div">
         <h3>ELEGI TU PLAN</h3>
         <p>y agendemos nuestra primera sesion</p>
       </div>
-        <div>
-        <h3>ASESORIA</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-          perferendis cupiditate voluptatum enim labore, repellendus beatae
-          corrupti iusto cumque adipisci, nisi, debitis magni assumenda dolorem!
-          Doloribus dignissimos ratione nostrum tempora.
-        </p>
-        <h3>INCLUYE</h3>
-        <p>loren lcdllr</p>
-        <p>descloren lcdllr</p>
-        <p>loren lcdllr</p>
-        <p>descloren lcdllr</p>
-        <p>loren lcdllr</p>
-        <p>descloren lcdllr</p>
-        <p>loren lcdllr</p>
-        <p>descloren lcdllr</p>
-        <button type="submit">Agenda tu asesoria</button>
       </div>
-      <div>
-        <h3>CONSULTORIA</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-          perferendis cupiditate voluptatum enim labore, repellendus beatae
-          corrupti iusto cumque adipisci, nisi, debitis magni assumenda dolorem!
-          Doloribus dignissimos ratione nostrum tempora.
-        </p>
-        <h3>INCLUYE</h3>
-        <p>loren lcdllr</p>
-        <p>descloren lcdllr</p>
-        <p>loren lcdllr</p>
-        <p>descloren lcdllr</p>
-        <p>loren lcdllr</p>
-        <p>descloren lcdllr</p>
-        <p>loren lcdllr</p>
-        <p>descloren lcdllr</p>
-        <button type="submit">Agenda tu asesoria</button>
+      <div className="as-Section3">
+        <div className="as-Section3-info">
+          <img src="../as-section3-consultoriasFrame.png" alt="" />
+          <h2>ASESORIA</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+            perferendis cupiditate voluptatum enim labore, repellendus beatae
+            corrupti iusto cumque adipisci, nisi, debitis magni assumenda dolorem!
+            Doloribus dignissimos ratione nostrum tempora.
+          </p>
+          <h3>INCLUYE</h3>
+          <p>loren lcdllr</p>
+          <p>descloren lcdllr</p>
+          <p>loren lcdllr</p>
+          <p>descloren lcdllr</p>
+          <p>loren lcdllr</p>
+          <p>descloren lcdllr</p>
+          <p>loren lcdllr</p>
+          <p>descloren lcdllr</p>
+          <button type="submit">AGENDA TU ASESORIA</button>
+        </div>
+        <div class="as-Section3-info">
+          <img src="../as-section3-aseoriasFrame.png" alt="" />
+          <h2>CONSULTORIA</h2>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+            perferendis cupiditate voluptatum enim labore, repellendus beatae
+            corrupti iusto cumque adipisci, nisi, debitis magni assumenda dolorem!
+            Doloribus dignissimos ratione nostrum tempora.
+          </p>
+          <h3>INCLUYE</h3>
+          <p>loren lcdllr</p>
+          <p>descloren lcdllr</p>
+          <p>loren lcdllr</p>
+          <p>descloren lcdllr</p>
+          <p>loren lcdllr</p>
+          <p>descloren lcdllr</p>
+          <p>loren lcdllr</p>
+          <p>descloren lcdllr</p>
+          <button type="submit">AGENDA TU ASESORIA</button>
+        </div>
       </div>
-      <div>
-        <h3>Auditoria de tus redes sociales</h3>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque porro
-          cum dolores similique! Rem temporibus velit error enim rerum?
-          Consequatur accusamus ratione voluptates magnam eos esse temporibus,
-          dolorem doloremque dignissimos?
-        </p>
+      <div class="as-Section3">
+        <div class="as-Section3-cards">
+          <div className="as-section3-card-img"> <img src="../icon-flechon.png" alt="" /></div>
+          <h3>Auditoria de tus redes sociales</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <hr />
+        <div className="as-Section3-cards">
+          <div className="section3-card-img"> <img src="../icon-flechon.png" alt="" /></div>
+          <h3>Imagen de tu marca</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <hr />
+        <div className="as-Section3-cards">
+          <div className="section3-card-img"> <img src="../icon-flechon.png" alt="" /></div>
+          <h3>Creá tu estrategia de redes sociales</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <hr />
+        <div className="as-Section3-cards">
+          <div className="section3-card-img"> <img src="../icon-flechon.png" alt="" /></div>
+          <h3>Sinergia</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          </p>
+        </div>
+
       </div>
-      <div>
-        <h3>Imagen de tu marca</h3>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque porro
-          cum dolores similique! Rem temporibus velit error enim rerum?
-          Consequatur accusamus ratione voluptates magnam eos esse temporibus,
-          dolorem doloremque dignissimos?
-        </p>
-      </div>
-      <div>
-        <h3>Creá tu estrategia de redes sociales</h3>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque porro
-          cum dolores similique! Rem temporibus velit error enim rerum?
-          Consequatur accusamus ratione voluptates magnam eos esse temporibus,
-          dolorem doloremque dignissimos?
-        </p>
-      </div>
-      <div>
-        <h3>Sinergia</h3>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque porro
-          cum dolores similique! Rem temporibus velit error enim rerum?
-          Consequatur accusamus ratione voluptates magnam eos esse temporibus,
-          dolorem doloremque dignissimos?
-        </p>
-      </div>
-      <div>
-        <h2>Hola, soy Nat!</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ad
-          beatae explicabo sed alias sint. Similique cupiditate iure natus quam
-          eligendi voluptate ipsam, perferendis ad nam ducimus facilis neque
-          nihil.
-        </p>
+      <div className="as-Section4">
+        <div className="as-Section4-info">
+          <h2>Hola, soy Nat!</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptatum dolore incidunt aspernatur tempora debitis, error assumenda fuga officia quidem, vitae sequi beatae expedita veritatis quam modi neque facilis ipsa eum consequatur voluptas nam reiciendis maxime labore. Incidunt voluptatem, esse voluptas alias rerum perferendis mollitia delectus velit voluptatum, exercitationem praesentium.
+          </p>
+        </div>
+        <div className="as-Section4-img">
+          <img src="./as-Section4-soynat.png" alt="" />
+        </div>
       </div>
     </>
   );

437a65589c3491b7d0af4d8467f88038a8ebeeb3
NachoEscobar <nachoescobar.ok@gmail.com>
2023-02-14 19:14:54 -0300

    falta terminar la page redes sociales

--- front/src/pages/Redes-sociales/redes.jsx
+++ front/src/pages/Redes-sociales/redes.jsx
@@ -1,88 +1,107 @@
 import React from "react";
+import "./redes.css";
 
 const RedesSociales = () => {
   return (
     <>
-      <div>
+      <div className="section1">
         <h4>COMO TE PODEMOS AYUDAR EN</h4>
         <h1>Redes Sociales</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         <button>SOLICITA MAS INFO</button>
       </div>
-      <div>
-        <h2>Generar un mundo sin Spam</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
-          repudiandae, sit natus libero provident inventore nihil autem
-          accusantium ut quis laboriosam quisquam doloribus in minus vero.
-          Suscipit eligendi qui obcaecati.
-        </p>
+      <div className="section2">
+        <div className="bloqueDeTexto">
+          <h2>Generar un mundo sin Spam</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+            repudiandae, sit natus libero provident inventore nihil autem
+            accusantium ut quis laboriosam quisquam doloribus in minus vero.
+            Suscipit eligendi qui obcaecati.
+          </p>
+        </div>
+        <div className="bloqueImagenes">
+          <img
+            src="CirculoDeCirculos.png"
+            alt="Fondo Decorativo"
+            className="circuloDeCirculos3"
+          />
+          <img
+            src="redesSociales-mobile.png"
+            alt="redes sociales mobile"
+            className="mobilePhone"
+          />
+        </div>
       </div>
-      <div>
-        <h3>PROCESO DE TRABAJO</h3>
+      <div className="section3">
+        <div className="cuadroDelMedio2">
+          <h3>PROCESO DE TRABAJO</h3>
+        </div>
       </div>
-      <div>
-        <h4>Auditar tus redes sociales</h4>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat quo
-          sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
-          laudantium error accusamus delectus repellat consectetur consequuntur
-          necessitatibus harum non.
-        </p>
-      </div>
-      <div>
-        <h4>Desarrollo de una estrategia</h4>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat quo
-          sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
-          laudantium error accusamus delectus repellat consectetur consequuntur
-          necessitatibus harum non.
-        </p>
-      </div>
-      <div>
-        <h4>Redaccion creativa</h4>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat quo
-          sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
-          laudantium error accusamus delectus repellat consectetur consequuntur
-          necessitatibus harum non.
-        </p>
-      </div>
-      <div>
-        <h4>Diseño de contenidos</h4>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat quo
-          sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
-          laudantium error accusamus delectus repellat consectetur consequuntur
-          necessitatibus harum non.
-        </p>
-      </div>
-      <div>
-        <h4>Moderacion</h4>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat quo
-          sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
-          laudantium error accusamus delectus repellat consectetur consequuntur
-          necessitatibus harum non.
-        </p>
-      </div>
-      <div>
-        <h4>Publicidad online</h4>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat quo
-          sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
-          laudantium error accusamus delectus repellat consectetur consequuntur
-          necessitatibus harum non.
-        </p>
-      </div>
-      <div>
-        <h4>Reportes de publicidad online</h4>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat quo
-          sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
-          laudantium error accusamus delectus repellat consectetur consequuntur
-          necessitatibus harum non.
-        </p>
+      <div className="section4">
+        <div>
+          <h4>Auditar tus redes sociales</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat
+            quo sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
+            laudantium error accusamus delectus repellat consectetur
+            consequuntur necessitatibus harum non.
+          </p>
+        </div>
+        <div>
+          <h4>Desarrollo de una estrategia</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat
+            quo sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
+            laudantium error accusamus delectus repellat consectetur
+            consequuntur necessitatibus harum non.
+          </p>
+        </div>
+        <div>
+          <h4>Redaccion creativa</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat
+            quo sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
+            laudantium error accusamus delectus repellat consectetur
+            consequuntur necessitatibus harum non.
+          </p>
+        </div>
+        <div>
+          <h4>Diseño de contenidos</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat
+            quo sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
+            laudantium error accusamus delectus repellat consectetur
+            consequuntur necessitatibus harum non.
+          </p>
+        </div>
+        <div>
+          <h4>Moderacion</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat
+            quo sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
+            laudantium error accusamus delectus repellat consectetur
+            consequuntur necessitatibus harum non.
+          </p>
+        </div>
+        <div>
+          <h4>Publicidad online</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat
+            quo sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
+            laudantium error accusamus delectus repellat consectetur
+            consequuntur necessitatibus harum non.
+          </p>
+        </div>
+        <div>
+          <h4>Reportes de publicidad online</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat
+            quo sit harum ipsa? Et soluta, animi eaque numquam eligendi quae
+            laudantium error accusamus delectus repellat consectetur
+            consequuntur necessitatibus harum non.
+          </p>
+        </div>
       </div>
     </>
   );

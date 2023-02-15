16da9923db0002e0038e92812f7e8075af2d6003
Nacho <nachoescobar.ok@gmail.com>
2023-02-15 20:37:33 -0300

    acomode  el footer

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -13,23 +13,21 @@ const Footer = () => {
   return (
     <footer className="footerContainer">
       <div className="suscribe">
-        <div className="containerHijo">
+        <form className="formuSuscribe" onSubmit={handleSubmit}>
           <div className="text">
             <p>QUIERO RECIBIR INFO</p>
           </div>
-          <form className="formuSuscribe" onSubmit={handleSubmit}>
-            <input
-              className="inputSuscribe"
-              type="email"
-              placeholder="E-mail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <button type="submit" className="btnSuscribe">
-              Subscribite
-            </button>
-          </form>
-        </div>
+          <input
+            className="inputSuscribe"
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button type="submit" className="btnSuscribe">
+            Subscribite
+          </button>
+        </form>
       </div>
       <div className="copyright">
         <p>©Copyright Carites Web . 2022</p>

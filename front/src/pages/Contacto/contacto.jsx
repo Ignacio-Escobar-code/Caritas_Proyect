7fe9c86ede7dc81688c96a96a297e9383e598bc1
Nacho <nachoescobar.ok@gmail.com>
2023-02-17 16:45:18 -0300

    terminado contacto

--- front/src/pages/Contacto/contacto.jsx
+++ front/src/pages/Contacto/contacto.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import "./contacto.css";
 
 const Contacto = () => {
   return (
     <>
-      <div>
+      <div className="cont-container">
         <h1>Marketing desde cero</h1>
         <h5>¿Querés potenciar tu emprendimiento o pyme?</h5>
+      </div>
+      <div className="cont-img-container">
+        <img
+          src="./img-contacto/Globo-carites.png"
+          alt=""
+          className="cont-globo-carites"
+        />
+        <img src="CirculoDeCirculos.png" alt="" className="cont-circulos" />
       </div>
     </>
   );
